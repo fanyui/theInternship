23613e7c46a3a876bccb1e9569dcba82aeb86d51
fanyui <fanyui@localhost.localdomain>
2018-01-05 11:12:08 +0100

    added section to searchdetails page

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/search', 'ApiSearchController@search');
+Route::get('/search/details/{company}', 'ApiSearchController@searchDetails');
+
+//these are responsible for the auto updating of the states when country is selected and cities when state is selected.
+Route::post('/country/states', 'ApiLocationController@ajax_country_states');
+Route::post('/state/cities', 'ApiLocationController@ajax_states_cities');
